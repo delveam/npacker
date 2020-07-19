da4136309c358ce0b5343d363d0ef1eec3795bb1
austin0209 <austinrandy0209@gmail.com>
2020-07-19 00:47:30 -0500

    did trimming and adding to canvas

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,24 +1,24 @@
 import IData from "./image.js";
 
 class CanvasNode {
-  private readonly x: number;
-  private readonly y: number;
-  private readonly width: number;
-  private readonly height: number;
-  img: IData;
+  readonly x: number;
+  readonly y: number;
+  readonly width: number;
+  readonly height: number;
+  idata: IData;
 
   constructor(
     x: number,
     y: number,
     w: number,
     h: number,
-    img = null as IData
+    idata = null as IData
   ) {
     this.x = x;
     this.y = y;
     this.width = w;
     this.height = h;
-    this.img = img;
+    this.idata = idata;
   }
 
   get area() {
@@ -42,10 +42,32 @@ class Canvas {
     this.clean();
   }
 
-  clean() {
-    this.nodes = this.nodes.filter((n) => n.area != 0);
+  private removeNode(toRemove: CanvasNode) {
+    for (let i = 0; i < this.nodes.length; i++) {
+      if (toRemove == this.nodes[i]) {
+        return this.nodes.splice(i, i);
+      }
+    }
   }
 
-  push(idata: IData) {}
+  clean() {
+    this.nodes = this.nodes.filter((n) => n.area != 0);
+    this.nodes.sort((a, b) => a.area - b.area);
+  }
+
+  push(idata: IData) {
+    this.nodes.forEach(n => {
+      let image = idata.img;
+      if (n.idata == null && n.width >= image.getWidth() && n.height >= image.getHeight()) {
+        let newNode = new CanvasNode(n.x, n.y, image.getWidth(), image.getHeight(), idata);
+        let left = new CanvasNode(n.x + image.getWidth(), n.y, n.width - image.getWidth(), image.getHeight());
+        let bot = new CanvasNode(n.x, n.y + image.getHeight(), n.width, n.height - image.getHeight());
+        this.removeNode(n);
+        this.nodes.push(newNode, left, bot);
+        this.clean();
+        return true;
+      }
+    });
+    return false;
+  }
 }
-module.exports.Canvas = Canvas;
--- src/image.ts
+++ src/image.ts
@@ -1,6 +1,58 @@
+import Jimp from "jimp";
+
 export default interface IData {
   name: string;
-  img: any;
+  img: Jimp;
   trimW: number;
   trimH: number;
+}
+
+export function trimmed(data: IData, border = 0) {
+  let image = data.img.clone();
+
+  // scan for left edge
+  for (let x = 0; x < image.getWidth(); x++) {
+    for (let y = 0; y < image.getHeight(); y++) {
+      let alpha = Jimp.intToRGBA(image.getPixelColor(x, y)).a;
+      if (alpha != 0) {
+        var left = x;
+      }
+    }
+  }
+  // scan for right edge
+  for (let x = image.getWidth() - 1; x >= 0; x--) {
+    for (let y = 0; y < image.getHeight(); y++) {
+      let alpha = Jimp.intToRGBA(image.getPixelColor(x, y)).a;
+      if (alpha != 0) {
+        var right = x;
+      }
+    }
+  }
+  // scan for top edge
+  for (let y = 0; y < image.getHeight(); y++) {
+    for (let x = 0; x < image.getWidth(); x++) {
+      let alpha = Jimp.intToRGBA(image.getPixelColor(x, y)).a;
+      if (alpha != 0) {
+        var top = y;
+      }
+    }
+  }
+  // scan for bot edge
+  for (let y = image.getHeight(); y >= 0; y--) {
+    for (let x = 0; x < image.getWidth(); x++) {
+      let alpha = Jimp.intToRGBA(image.getPixelColor(x, y)).a;
+      if (alpha != 0) {
+        var bot = y;
+      }
+    }
+  }
+
+  let newWidth = image.getWidth() - (image.getWidth() - right);
+  let newHeight = image.getHeight() - (image.getHeight() - bot);
+  image.crop(left, top, newWidth, newHeight);
+  if (border != 0) {
+    image = new Jimp(image.getWidth() + border, image.getHeight() + border)
+      .mask(image, 0, 0);
+  }
+  return { name: data.name, img: image, trimW: left, trimH: top };
 }
