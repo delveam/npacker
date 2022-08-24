5b07c595e61cabd564ca6112474f72ead598c7a2
Austin Aurelio <austin@delveam.com>
2022-08-23 22:14:12 -0500

    chore: use tabs instead of 2 spaces

--- src/index.ts
+++ src/index.ts
@@ -7,53 +7,53 @@ import processArgs from "./cli.js";
 import { readAllImages } from "./image.js";
 
 processArgs(process.argv.splice(2))
-  .then((args) => {
-    return readAllImages(args);
-  })
-  .then(async ({ images, args }) => {
-    images.sort((a: IData, b: IData) => {
-      let res = b.img.getHeight() - a.img.getHeight();
-      if (res == 0) {
-        res = b.img.getWidth() - a.img.getWidth();
-        if (res == 0) {
-          res = a.name.localeCompare(b.name);
-        }
-      }
-      return res;
-    });
+	.then((args) => {
+		return readAllImages(args);
+	})
+	.then(async ({ images, args }) => {
+		images.sort((a: IData, b: IData) => {
+			let res = b.img.getHeight() - a.img.getHeight();
+			if (res == 0) {
+				res = b.img.getWidth() - a.img.getWidth();
+				if (res == 0) {
+					res = a.name.localeCompare(b.name);
+				}
+			}
+			return res;
+		});
 
-    let canvasSize = 1;
-    let success = false;
-    while (!success) {
-      success = true;
-      let canvas = new Canvas(canvasSize, canvasSize, args.border);
-      for (let i = 0; i < images.length; i++) {
-        let img = images[i];
-        if (!canvas.push(img)) {
-          success = false;
-          canvasSize *= 2;
-          break;
-        }
-      }
-      if (success) {
-        try {
-          const image = await canvas.getPng();
-          image.write(args.output + "/" + args.filename + ".png");
-          console.log(
-            "Wrote PNG to: " + args.output + " as: " + args.filename + ".png"
-          );
-          const json = canvas.getJson(args.filename);
-          fs.writeFileSync(args.output + "/" + args.filename + ".json", json);
-          console.log(
-            "Wrote JSON to: " + args.output + " as: " + args.filename + ".json"
-          );
-        } catch (e) {
-          console.log("ERROR: Error writing png/json file!");
-        }
-      }
-    }
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+		let canvasSize = 1;
+		let success = false;
+		while (!success) {
+			success = true;
+			let canvas = new Canvas(canvasSize, canvasSize, args.border);
+			for (let i = 0; i < images.length; i++) {
+				let img = images[i];
+				if (!canvas.push(img)) {
+					success = false;
+					canvasSize *= 2;
+					break;
+				}
+			}
+			if (success) {
+				try {
+					const image = await canvas.getPng();
+					image.write(args.output + "/" + args.filename + ".png");
+					console.log(
+						"Wrote PNG to: " + args.output + " as: " + args.filename + ".png"
+					);
+					const json = canvas.getJson(args.filename);
+					fs.writeFileSync(args.output + "/" + args.filename + ".json", json);
+					console.log(
+						"Wrote JSON to: " + args.output + " as: " + args.filename + ".json"
+					);
+				} catch (e) {
+					console.log("ERROR: Error writing png/json file!");
+				}
+			}
+		}
+	})
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	});
